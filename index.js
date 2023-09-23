42ca8ae221252fada09703449be514ddc91fb65d
Xyrsto <rodrigtomas@gmail.com>
2023-09-23 15:50:57 +0100

    teste base de dados

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+//utilização do express para criar e configurar o servidor
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+const uri = 'mongodb+srv://admin:admin@cluster0.ujeva17.mongodb.net/?retryWrites=true&w=majority'
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+//conexão com a base de dados
+async function connect(){
+  try{
+    await mongoose.connect(uri)
+    console.log('Conectado à base de dados')
+  }
+  catch{
+    console.error()
+  }
+}
+
+//executa a função connect para conectar à base de dados
+connect()
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
 })
+
