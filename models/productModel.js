4413b228def07d5731b67aaaf59c3781edfeaf46
RBMGCardoso <rubenbmgcardoso@gmail.com>
2023-09-27 19:36:47 +0100

    AHHHHH

--- models/productModel.js
+++ models/productModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const productSchema = mongoose.Schema(
     {
+        _id: mongoose.Schemas.Types.ObjectId,
         name:{
             type: String,
             required: [true, "Introduza um nome válido"]
