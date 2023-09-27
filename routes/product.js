c9cef4a855fe2ef7c27343af34c980553f046442
Xyrsto <rodrigtomas@gmail.com>
2023-09-27 15:23:07 +0100

    Test product add

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,8 @@ const epxress = require('express');
 const router = epxress.Router();
 
 const ProductController = require('../controllers/productController')
+const upload = require('../config/multer')
 
-router.post('/', ProductController.create)
+router.post('/', upload.single("file"), ProductController.create)
 
 module.exports = router;
