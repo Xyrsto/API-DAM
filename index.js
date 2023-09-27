f89bbab8fcae5ce6e90609bc04644e435ad1e7bf
Xyrsto <rodrigtomas@gmail.com>
2023-09-27 15:46:33 +0100

    show error message

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const User = require('./models/userModel')
 const Product = require('./models/productModel')
+const multer=require('multer');
 const cors = require('cors')
 const app = express()
 
