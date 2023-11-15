b7709076d55cd569ae01040142aa787181e760f6
Xyrsto <73367973+Xyrsto@users.noreply.github.com>
2023-11-15 23:49:52 +0000

    registers and logins are working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const User = require('./models/userModel')
+const bcrypt = require('bcrypt')  
 const Color = require('./models/colorModel')
 const cors = require('cors')
 const app = express()
@@ -39,19 +40,58 @@ app.post('/addColor', async(req, res) =>{
   }
 })
 
-//teste adicionar user
-app.post('/addUser', async(req, res) =>{
-  try{
-    const user = await User.create(req.body)
-    console.log('User created:', user); // Log user object for debugging
-    res.status(200).json(user)
-  }
-  catch(err){
-    console.log(err.message)
-    res.status(500).json({msg: err.message})
-  }
-})
+app.post('/addUser', async (req, res) => {
+  try {
+    const user = req.body;
 
+    const existingUsername = await User.findOne({ username: user.username });
+    if (existingUsername) {
+      return res.status(400).json({ msg: 'Username is already taken' });
+    }
+
+    const existingEmail = await User.findOne({ email: user.email });
+    if (existingEmail) {
+      return res.status(400).json({ msg: 'Email is already in use' });
+    }
+
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+
+    const createdUser = await User.create(user);
+    res.status(200).json(createdUser);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: err.message });
+  }
+});
+
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (user) {
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      console.log('Retrieved User:', user.username);
+      console.log(password); 
+      console.log(user.password); 
+
+      if (passwordMatch) {
+        res.status(200).json({ message: 'Login successful', user });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Internal server error' });
+  }
+});
 
 app.get('/getColor', async(req, res) => {
   try{
